d0400a3a582437e6edff25aa04fa9b0bccbb6b59
Joel Zamboni <joel.zamboni@webera.com>
2021-05-22 18:04:53 +0000

    testing

--- config.sh
+++ config.sh
@@ -1,23 +1,27 @@
 APPS=(
-  nodejs
+  nodejs-1
+  nnodjs-2
   pythonapp
   javaapp
 )
 
 declare -A IMAGES=(
-  [nodejs]="imagerepository/nodejs"
+  [nodejs-1]="ïmagerepository/nodejs-n"
+  [nodejs-2]="imagerepository/nodejs"
   [pythonapp]="imagerepository/pythonapp"
   [javaapp]="anotherimagerepository/javaapp"
 )
 
 declare -A REPLICAS=(
-  [nodejs]="2"
+  [nodejs-1]="2"
+  [nodejs-2]="2"
   [pythonapp]="1"
   [javaapp]="3"
 )
 
 declare -A PORTS=(
-  [nodejs]="3000"
+  [nodejs-1]="3000"
+  [nodejs-2]="3000"
   [pythonapp]="5000"
   [javaapp]="8080"
 )
